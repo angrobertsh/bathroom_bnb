f3c613ec8c48f363abab10478a1283ea43e938a8
Robert Ang <angrobertsh@gmail.com>
2016-12-16 10:26:16 -0800

    maybe set up file structure properly

---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = ({children}) => (
+  <div className="App">
+    <div className="bodycontainer">
+      {children}
+    </div>
+  </div>
+);
+
+export default App;
---
+++ frontend/components/router/router.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import App from '../app';
+
+class AppRouter extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.routerconst = (
+      <Router history={ hashHistory } onUpdate={() => window.scrollTo(0, 0)}>
+        <Route path="/" component={ App }>
+        </Route>
+      </Router>
+    )
+  }
+
+  render() {
+    return this.routerconst;
+  }
+}
+
+export default AppRouter;
